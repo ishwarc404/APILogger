f3e31b14db5bc54bc358b57c8155e1d75236bbb1
Ishwar Choudhary <ishwar@github.com>
2020-05-02 10:51:36 +0530

    minor changes

--- backend/receiving.py
+++ backend/receiving.py
@@ -98,6 +98,6 @@ def clearLogs():
     return("200")
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', debug=True, port=80)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=80)
+    # app.run(debug=True)
 
